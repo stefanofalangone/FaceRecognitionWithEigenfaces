7cdc678e141efd55cfbc703802cc865b76597af5
Davide <dadedevita@gmail.com>
2020-08-17 11:13:36 +0200

    merge, Dave

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
 
 
 print("Hello World!!")
-
-print("First attempt of commit")
-
-print("2nd attempt of commit")
-
-print("3rd attempt of commit")
-
-print("prova")
-
-print("4th attempt of commit")
-
-print("aggiungi pure tu qualcosa")
