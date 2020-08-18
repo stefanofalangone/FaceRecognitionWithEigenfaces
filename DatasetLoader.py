91f6b7ffe285bd6ee1fdca89745512e6672cfdc4
stefanofalangone <stefanofalangone@gmail.com>
2020-08-18 11:56:06 +0200

    Added setup

--- DatasetLoader.py
+++ DatasetLoader.py
@@ -4,11 +4,19 @@ import numpy as np
 class DatasetLoader:
 
     path = "/"
-    n_images = 400
-    width_images = 92
-    height_images = 112
-    TrainingSet = None
-    TestSet = None
+    n_directories = 0
+    n_images_per_directory = 0
+    width_images = 0
+    height_images = 0
+    training_set = None
+    test_set = None
 
     def __init__(self):
         print("create the dataset Loader\n")
+        self.setupImgFormat(self, 40, 10, 92, 112)
+
+    def setupImgFormat(self, n_directories, n_images_per_directory, width_images, height_images):
+        self.n_directories = n_directories
+        self.n_images_per_directory = n_images_per_directory
+        self.width_images = width_images
+        self.height_images = height_images
