06719b0a228dec1e8f7006164009216f1c4607d4
Davide <dadedevita@gmail.com>
2020-08-18 14:47:27 +0200

    Rimozione delle dimensioni dell'immagine dal setup, divisione del setup in due per generalizzare

--- DatasetLoader.py
+++ DatasetLoader.py
@@ -1,22 +1,37 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+"""This class shall be responsible of the correct reading of the 'dataset' directory.
+    In our specific case, there are 40 directories (one for each subject) of 10 images each.
+    Every image is 92x112 pixels of 8-bit grey scale. (Each img say this anyway)
+"""
 class DatasetLoader:
 
-    path = "/"
+    path = "dataset/"
     n_directories = 0
     n_images_per_directory = 0
     width_images = 0
     height_images = 0
+    should_check_pixel_format = True
     training_set = None
     test_set = None
 
-    def __init__(self):
+    def __init__(self, path="dataset/"):
         print("create the dataset Loader\n")
-        self.setupImgFormat(self, 40, 10, 92, 112)
+        self.path = path
+        self.setupDirectoryFormat(40, 10)
+        self.setupImgFormat(92, 112)
 
-    def setupImgFormat(self, n_directories, n_images_per_directory, width_images, height_images):
+    def setupDirectoryFormat(self, n_directories=40, n_images_per_directory=10):
         self.n_directories = n_directories
         self.n_images_per_directory = n_images_per_directory
-        self.width_images = width_images
-        self.height_images = height_images
+
+    def setupImgFormat(self, width_img=92, height_img=112):
+        self.width_images = width_img
+        self.height_images = height_img
+        should_check_pixel_format = False
+
+    def load(self):
+        for subj in range(1, 41, 1):
+            for pic in range (1, 11, 1):
+                print("subj ", subj, ". Pic ", pic)
