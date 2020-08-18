929af2c54cdde43b4298de3bb27d2f46e203ce34
Maurizio <mau.moraca@gmail.com>
2020-08-18 16:21:47 +0200

    updated DatasetLoader

--- DatasetLoader.py
+++ DatasetLoader.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 class DatasetLoader:
 
-    path = "/"
+    path = "./dataset/"
     n_directories = 0
     n_images_per_directory = 0
     width_images = 0
@@ -13,10 +14,52 @@ class DatasetLoader:
 
     def __init__(self):
         print("create the dataset Loader\n")
-        self.setupImgFormat(self, 40, 10, 92, 112)
+        self.setupImgFormat(40, 10, 92, 112)
+        self.training_set, self.test_set = self.extractTrainingsetTestset(70)
+
+        self.showImage(self.training_set[:,0])
 
     def setupImgFormat(self, n_directories, n_images_per_directory, width_images, height_images):
         self.n_directories = n_directories
         self.n_images_per_directory = n_images_per_directory
         self.width_images = width_images
         self.height_images = height_images
+
+    def readPgm(self, path):
+        pgmf = open(path, 'rb')
+        assert pgmf.readline() == b'P5\n'
+        width, height = [int(i) for i in pgmf.readline().split()]
+        max_val = int(pgmf.readline())
+        assert max_val <= 255
+
+        vectorialized_image = []
+        for k in range(height):
+            for j in range(width):
+                vectorialized_image.append(ord(pgmf.read(1)))
+
+        return np.array(vectorialized_image, dtype='float')
+
+    def extractTrainingsetTestset(self, trainingPercentage):
+        trainingSet = []
+        testSet = []
+        data_list = [i for i in range(1, self.n_images_per_directory+1)]
+        number_of_training_images_per_directories = int((self.n_images_per_directory * trainingPercentage)/100)
+        number_of_testing_images_per_directories = self.n_images_per_directory - number_of_training_images_per_directories
+
+        for i in range(1, self.n_directories+1):
+            test_list = random.sample(data_list, number_of_testing_images_per_directories)
+            train_list = list(set(data_list) - set(test_list))
+            path = self.path + 's'+str(i)+'/'
+
+            for j in train_list:
+                trainingSet.append(self.readPgm(path+str(j)+'.pgm'))
+
+            for k in test_list:
+                testSet.append(self.readPgm(path+str(k)+'.pgm'))
+
+
+        return np.stack(trainingSet, axis=-1), np.stack(testSet, axis=-1)
+
+    def showImage(self, vectorialized_image):
+        plt.imshow(vectorialized_image.reshape((112, 92)), cmap='gray')
+        plt.show()
