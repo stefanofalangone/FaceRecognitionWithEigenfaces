cce1485d1de542167b6320d5d9818fd2ca0b97c2
stefanofalangone <stefanofalangone@gmail.com>
2020-08-26 14:24:52 +0200

    Refactored for main dataset recognition

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import numpy as np
 
 path_dataset_yale = "./datasetYale/Yale_Cropped_Dataset/" #images are 168 x 192, 38 dir
 path_standard_dataset = "./dataset/" # images are 92 x 112, 40 dir
-width_image, height_image, n_directories  = 92, 112, 40
-width_image, height_image, n_directories  = 168, 192, 38
+width_image, height_image, n_directories  = 92, 112, 40 #standard one
+#width_image, height_image, n_directories  = 168, 192, 38 #for yale
 vectorial_image_size = width_image * height_image
-dataset = DatasetLoader( path_dataset_yale, width_image, height_image, n_directories )
+dataset = DatasetLoader( path_standard_dataset, width_image, height_image, n_directories )
 training_set = dataset.getTrainingSet()
 test_set = dataset.getTestSet()
 training_set_labels = dataset.getTrainingSetLabels()
